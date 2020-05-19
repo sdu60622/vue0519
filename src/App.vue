24c54598ad5f533eb4c01b438efffc11d2c03ad4
sdu60622 <sdu60622@gmail.com>
2020-05-19 11:06:05 +0800

    up

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  #app
+    Top
+    Marquee
+    #content
+      Content
+    #footer
+      Footer
+</template>
+
+<script>
+import Top from './components/Top.vue'
+import Marquee from './components/Marquee.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Top,
+    Marquee,
+    Content,
+    Footer
+  }
+}
+</script>
---
+++ src/components/Marquee.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  #Marquee
+    marquee 大家來購物網！精品商品特價大拍賣！要買要快
+</template>
+
+<script>
+export default {
+  data () {
+
+  }
+}
+</script>
